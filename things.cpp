ddaf36efd4adf48514570267ade8dbc89ef262ed
Morsdors <69536773+Morsdors@users.noreply.github.com>
2021-05-25 15:16:50 +0200

    Dodane metody w Camera things.cpp

--- things.cpp
+++ things.cpp
@@ -21,17 +21,44 @@ Worm::update(float speed, float angle_speed, double _time){
   if (y != -1000000000){
     pos = glm::vec3(x, y, z);
   }
+  camera.update_pos(glm::vec3(x+5, y+5, z+5););  //camera zainicjalizowana w mainie
+  
 }
 
 
 Bullet::Bullet(glm::vec3 pos, float angle_x, float angle_y){
-  this->pos = pos;
+  this -> pos = pos;
   float speed_val = 10;
-  this->speed = vec3(speed_val*cos(angle_x)*cos(angle_y), speed_val*sin(angle_y), speed_val*sin(angle_x)*cos(angle_y));
+  this -> speed = vec3(speed_val*cos(angle_x)*cos(angle_y), speed_val*sin(angle_y), speed_val*sin(angle_x)*cos(angle_y));
   this -> angle_y = nullptr;
   this -> angle_x = nullptr;
 }
+////////////dodane
+Camera::Camera(){
+  walking_mode=true;
+  this -> angle_x = 0;
+  this -> angle_y = nullptr;
+}
 
+Camera::change_mode(){
+  //switch to different mode
+}
+
+Camera::get_angle_x(){
+  return this -> angle_x;
+}
+
+Camera::get_angle_y(){
+  return this -> angle_y;
+}
+Camera::get_mode(){
+  return this -> walking_mode;
+}
+
+Camera::update_pos(_pos){
+    this -> pos = _pos;
+}
+///////////koniec
 
 Board::Board(){
 

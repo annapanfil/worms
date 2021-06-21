d957ce24cf94b8baabea8902bdb6fb5724f0d3bd
panka134 <134panka@gmail.com>
2021-06-21 12:30:19 +0200

    prepare f_shader for multiple lights

--- f_shader.glsl
+++ f_shader.glsl
@@ -6,40 +6,33 @@ uniform sampler2D texMapNormal;
 
 out vec4 pixelColor; //Output variable. Almost final pixel color
 
-// in vec4 i_color;
-// in vec4 n;
 in vec4 l;
 in vec4 v;
 in vec2 i_texc;
 
-float toonify(float a, float s){
-	return round(s*a)/s;
+
+vec4 calc_light(vec4 light_position, vec4 normal, vec4 vertex, vec4 surface_color, vec4 reflect_color){
+	vec4 reflect = reflect(-light_position, normal); //w przestrzeni oka, bo l i n są w tej przestrzeni
+
+	float nl = clamp(dot(normal, light_position), 0,1); // cos of the angle between normal and light source vectors
+	float alfa = 25; //how much the light is scattered
+	float rv = pow(clamp(dot(reflect,vertex), 0, 1), alfa);
+
+	//L = ka*la + kd*ld*nl + ks*ls*pow(rv, alfa)
+	return vec4(surface_color.rgb * nl, surface_color.a) + vec4(reflect_color.rgb*rv, 0);
 }
 
+
 void main(void) {
-	float brighteness = 1.5;
-
-	vec4 kd = texture(texMapColor,i_texc) * brighteness; //kolor powierzchni
-
-	vec4 ml = normalize(l);
-	// vec4 mn = normalize(n);
+	// float brighteness = 1.5;
 	vec4 mn = normalize(vec4(texture(texMapNormal, i_texc).rgb*2-1, 0)); //pobranie wektorów normalnych tekstury i znormalizowanie ich do przedziału [-1,1]
 	vec4 mv = normalize(v);
-	vec4 mr = reflect(-ml, mn); //w przestrzeni oka, bo l i n są w tej przestrzeni
 
-	// vec4 ks = texture(texMapReflect,i_texc);	//kolor światła odbitego
-	vec4 ks = vec4(1,1,1,1);
+	vec4 kd = texture(texMapColor,i_texc); //kolor powierzchni
+	vec4 ks = texture(texMapReflect,i_texc);	//kolor światła odbitego
 
-	float nl = clamp(dot(mn, ml), 0,1); // cos kąta
-	float alfa = 25;
-	float rv = pow(clamp(dot(mr,mv), 0, 1), alfa);
 
-	//cieniowanie kreskówkowe
-	// nl = toonify(nl, 4);
-	// rv = toonify(rv, 4);
-
-//L = ka*la + kd*ld*nl + ks*ls*pow(rv, alfa)
-	pixelColor=vec4(kd.rgb * nl, kd.a) + vec4(ks.rgb*rv, 0);
+	pixelColor=calc_light(l, mn, mv, kd, ks);
 
 
 
